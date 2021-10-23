d1a00c2eda9584713a1da18ddda5e65d926eb5cd
dwiprasetya <dwiprasetya93@gmail.com>
2021-10-23 21:06:51 +0700

    using docker compose volume

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>WOIIII Hello docker</p>
+        <p>tra Hello docker</p>
         <a
           className="App-link"
           href="https://reactjs.org"
